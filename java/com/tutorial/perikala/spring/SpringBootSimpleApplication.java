0498fe30224bf954cc67f1da1fd60d0a8fdddbe3
VIJAYMANOHAR PERIKALA <vijay@perikala.com.my>
2021-08-15 06:20:23 +0530

    added pom.xml with spring boot configuration

---
+++ java/com/tutorial/perikala/spring/SpringBootSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.perikala.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSimpleApplication.class, args);
+	}
+
+}
